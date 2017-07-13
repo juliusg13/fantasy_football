66ed0607b92875dc386831d2b91dcd12839ed2d4
Júlíus Geir Gíslason <juliusg13@ru.is>
2017-07-13 19:14:20 +0000

    new images added, functionality between scenes

---
+++ Assets/scripts/general_game_manager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class general_game_manager : MonoBehaviour {
+    public GameObject gm;
+    public string user;
+    public string money;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Awake() {
+        DontDestroyOnLoad(gm);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
