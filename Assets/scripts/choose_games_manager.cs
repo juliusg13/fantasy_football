2eff49365b9d601f1afcbc579c72aa67a676069e
Júlíus Geir Gíslason <juliusg13@ru.is>
2017-08-24 23:55:03 +0000

    Working highlight on game menu. Still need to max it at 7.

--- Assets/scripts/choose_games_manager.cs
+++ Assets/scripts/choose_games_manager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class choose_games_manager : MonoBehaviour {
     public GameObject choose1, choose2;
@@ -8,6 +9,7 @@ public class choose_games_manager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         gm = GameObject.Find("MainGameManager");
+        showGames1(true);
 	}
 	
 	// Update is called once per frame
@@ -26,6 +28,22 @@ public class choose_games_manager : MonoBehaviour {
         } else {
             choose1.SetActive(false);
             choose2.SetActive(true);
+        }
+    }
+    public void highlightChosenGame(GameObject btn) {
+        GameObject parent = btn.transform.parent.gameObject;
+        disableHighlightOnSiblings(parent);
+        if (btn.name == "tieBtn") {
+            btn.GetComponent<Image>().color = new Color32(255, 0, 0, 255);
+        } else {
+            disableHighlightOnSiblings(parent.transform.parent.gameObject);
+            btn.transform.parent.gameObject.GetComponent<Image>().color = new Color32(255, 0, 0, 255);
+        }
+    }
+
+    private void disableHighlightOnSiblings(GameObject parent) {
+        foreach(Transform child in parent.transform) {
+            child.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
         }
     }
 
