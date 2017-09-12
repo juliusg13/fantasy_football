b5758776b4ac8e7f018ea439a011bfb5fc3844a9
Júlíus Geir Gíslason <juliusg13@ru.is>
2017-09-12 21:53:05 +0000

    fixed deselect, preparing max 7 games

--- Assets/scripts/choose_games_manager.cs
+++ Assets/scripts/choose_games_manager.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 
 public class choose_games_manager : MonoBehaviour {
     public GameObject choose1, choose2;
+    private Color highlightColor;
     GameObject gm;
-	// Use this for initialization
-	void Start () {
+    int[] chosenGames;
+    // Use this for initialization
+    void Start() {
         gm = GameObject.Find("MainGameManager");
         showGames1(true);
-	}
+        highlightColor = new Color(1f, 0f, 0f, 1f);
+        chosenGames = new int[6];
+        initArray();
+        checkArraySize();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,24 +37,58 @@ public class choose_games_manager : MonoBehaviour {
             choose2.SetActive(true);
         }
     }
-    public void highlightChosenGame(GameObject btn) {
-        GameObject parent = btn.transform.parent.gameObject;
-        disableHighlightOnSiblings(parent);
-        if (btn.name == "tieBtn") {
-            btn.GetComponent<Image>().color = new Color32(255, 0, 0, 255);
-        } else {
-            disableHighlightOnSiblings(parent.transform.parent.gameObject);
-            btn.transform.parent.gameObject.GetComponent<Image>().color = new Color32(255, 0, 0, 255);
+
+    public void highlightChosenGameHandler(GameObject btn) {
+        int firstFreeSpot = checkArraySize();
+        if(firstFreeSpot != -1) { 
+            GameObject parent = btn.transform.parent.gameObject;
+            if(btn.name != "tieBtn" && parent.GetComponent<Image>().color != highlightColor) {
+                disableHighlightOnSiblings(parent.transform.parent.gameObject);
+                highlightChosenGame(parent);
+            }
+            else if (btn.name == "tieBtn" && btn.GetComponent<Image>().color != new Color(1f, 0f, 0f, 1f)) {
+                disableHighlightOnSiblings(parent);
+                highlightChosenGame(btn);
+            } 
+            else {
+                if (btn.name == "tieBtn") {
+                    disableHighlightOnSiblings(parent);
+                }
+                if (parent.name == "team1Img" || parent.name == "team2Img") {
+                    disableHighlightOnSiblings(parent.transform.parent.gameObject);
+                }
+            }
         }
+        else {
+            Debug.Log("NO SPACE, POP UP WINDOW.");
+        }
+    }
+
+    private void highlightChosenGame(GameObject btn) {
+        btn.GetComponent<Image>().color = highlightColor;
     }
 
     private void disableHighlightOnSiblings(GameObject parent) {
         foreach(Transform child in parent.transform) {
-            child.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            child.GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
         }
     }
 
     public void callPreviousScene() {
         gm.GetComponent<general_game_manager>().callNewTablesScreen();
+    }
+
+    private void initArray() {
+        for(int i = 0; i < 6; i++) {
+            chosenGames[i] = 11;
+        }
+    }
+
+    private int checkArraySize() {
+        int firstEmpty;
+        firstEmpty = System.Array.IndexOf(chosenGames, 11);
+
+        Debug.Log(firstEmpty);
+        return firstEmpty;
     }
 }
---
+++ Assets/scripts/max_games_chosen_dialog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class max_games_chosen_dialog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    [MenuItem("Example/Place Selection On Surface")]
+    static void CreateWizard() {
+        Transform[] transforms = Selection.GetTransforms(SelectionMode.Deep |
+                SelectionMode.ExcludePrefab | SelectionMode.Editable);
+
+        if (transforms.Length > 0 &&
+            EditorUtility.DisplayDialog("Max 7 games.",
+                "You can only bet on 7 games.", "Ok")) {
+            foreach (Transform transform in transforms) {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, -Vector3.up, out hit)) {
+                    transform.position = hit.point;
+                    Vector3 randomized = Random.onUnitSphere;
+                    randomized = new Vector3(randomized.x, 0F, randomized.z);
+                    transform.rotation = Quaternion.LookRotation(randomized, hit.normal);
+                }
+            }
+        }
+    }
+}
